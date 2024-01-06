0fb1001900ec31620c18cd95a59ca2666091a2f0
elneko42 <franson177@gmail.com>
2024-01-06 15:19:37 +0100

    controlador crear mesa

---
+++ app/Http/Controllers/MesasController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Mesa;
+
+class MesasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'tipo_de_mesa' => 'required',
+            'numero_de_asientos' => 'required|integer',
+            'numero_de_mesa' => 'required|string',
+        ]);
+    
+        $mesa = new Mesa();
+        $mesa->tipo = $request->tipo_de_mesa;
+        $mesa->asientos = $request->numero_de_asientos;
+        $mesa->numero_mesa = $request->numero_de_mesa;
+        $mesa->save();
+    
+        return response()->json(['message' => 'Mesa creada con éxito', 'data' => $mesa], 201);
+    }
+    
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\MesasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/mesas', [MesasController::class, 'store']);
+
 
