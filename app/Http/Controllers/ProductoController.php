e9fc8741edcd2c34a01685e002c479b450fbf7b3
elneko42 <franson177@gmail.com>
2024-01-11 16:53:26 +0100

    actualizar producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -37,14 +37,14 @@ class ProductoController extends Controller
             'nombre' => 'required|max:255',
             'precio_interior' => 'required|numeric',
             'precio_terraza' => 'required|numeric',
-           
+
         ]);
-    
+
         // Procesamiento y almacenamiento de la imagen
         if ($request->hasFile('imagen')) {
             $imagenPath = $request->file('imagen')->store('imagenes', 'public');
         }
-    
+
         // Crear el producto
         $producto = Producto::create([
             'tipo_producto_id' => $validatedData['tipo_producto_id'],
@@ -53,7 +53,7 @@ class ProductoController extends Controller
             'precio_terraza' => $validatedData['precio_terraza'],
             'imagen' => $imagenPath ?? null,
         ]);
-    
+
         return response()->json($producto, 201);
     }
 
@@ -62,7 +62,13 @@ class ProductoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $producto = Producto::find($id);
+
+        if (is_null($producto)) {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+
+        return response()->json($producto);
     }
 
     /**
@@ -78,8 +84,25 @@ class ProductoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
-    }
+
+
+        $producto = Producto::find($id);
+
+        if (is_null($producto)) {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+
+        // Si quieres manejar la carga de archivos, asegúrate de validar y almacenar el archivo correctamente.
+        if ($request->hasFile('imagen')) {
+            $imagenPath = $request->file('imagen')->store('imagenes_productos', 'public');
+            // Eliminar la imagen antigua si es necesario
+            // Storage::delete('public/' . $producto->imagen);
+            $validatedData['imagen'] = $imagenPath;
+        }
+        $producto->tipo_producto_id = $request->tipo_producto;
+        $producto->update($request->all());
+
+        return response()->json($producto);    }
 
     /**
      * Remove the specified resource from storage.
